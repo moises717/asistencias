048f514218d1544df32821e5fa275a284a8545f7
Moises Barillas <moisesrocha717@gmail.com>
2024-01-27 22:07:37 -0600

    Update navbar buttons styling

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -7,26 +7,26 @@ import { IconExcel } from '../icons';
 
 export const Navbar = () => {
 	return (
-		<div className='w-full flex pb-2 gap-2 justify-end'>
+		<div className='w-full flex pb-2 gap-4 justify-end'>
 			<Link href='/miembros'>
-				<Button size='sm' variant='secondary' className='flex gap-1'>
+				<Button variant='secondary' className='flex gap-1'>
 					<span className='hidden md:block'>Miembros </span> <ListBulletIcon />
 				</Button>
 			</Link>
 			<Link href='/asistencia'>
-				<Button size='sm' variant='secondary' className='flex gap-1 '>
+				<Button variant='secondary' className='flex gap-1 '>
 					<span className='hidden md:block'>Reportes</span>
 					<IconExcel className='w-4 h-4' />
 				</Button>
 			</Link>
 			<Link href='/asistencia/registrar'>
-				<Button size='sm' variant='secondary' className='flex gap-1 '>
+				<Button variant='secondary' className='flex gap-1 '>
 					<span className='hidden md:block'>Registrar asistencia</span>
 					<PersonIcon />
 				</Button>
 			</Link>
 			<Link href='/miembros/agregar-miembro'>
-				<Button size='sm' className='flex gap-1 '>
+				<Button className='flex gap-1 '>
 					<span className='hidden md:block'>Nuevo miembro </span> <PlusCircledIcon />
 				</Button>
 			</Link>
