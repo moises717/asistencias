197bb0fb06e431e771fe96b10e8ff2a1d18563ab
Moises Barillas <moisesrocha717@gmail.com>
2024-01-28 18:44:13 -0600

    Add IconLogout component to icons/index.tsx and implement logout functionality in navbar/index.tsx

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -24,3 +24,23 @@ export function IconFile(props: React.SVGProps<SVGSVGElement>) {
 		</svg>
 	);
 }
+export function IconLogout(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			{...props}
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			className='lucide lucide-log-out'
+		>
+			<path d='M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4' />
+			<polyline points='16 17 21 12 16 7' />
+			<line x1='21' x2='9' y1='12' y2='12' />
+		</svg>
+	);
+}
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,11 +1,18 @@
 'use client';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { ListBulletIcon, PersonIcon, PlusCircledIcon } from '@radix-ui/react-icons';
 
-import { Button } from '../ui/button';
-import { IconExcel } from '../icons';
+import { Button } from '@/components/ui/button';
+import { IconExcel, IconLogout } from '@/components/icons';
+import { pb } from '@/lib/pb';
 
 export const Navbar = () => {
+	const router = useRouter();
+	const logout = () => {
+		pb.authStore.clear();
+		router.push('/');
+	};
 	return (
 		<div className='w-full flex pb-2 gap-4 justify-end md:justify-between'>
 			<Link href='/'>
@@ -30,6 +37,9 @@ export const Navbar = () => {
 					<span className='hidden sm:block'>Nuevo miembro </span> <PlusCircledIcon />
 				</Button>
 			</Link>
+			<Button size='icon' variant='destructive' className='flex gap-1' onClick={logout}>
+				<IconLogout />
+			</Button>
 		</div>
 	);
 };
